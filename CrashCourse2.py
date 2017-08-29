5441d05dc4919e01e836f8c6b9f1a207ca088076
sarahjudithwright <31255826+sarahjudithwright@users.noreply.github.com>
2017-08-28 20:04:18 -0400

    Add files via upload

--- CrashCourse2.py
+++ CrashCourse2.py
@@ -4,3 +4,31 @@ import xlwt
 
 
 workbook = xlrd.open_workbook('NYGasPrices')
+
+
+
+
+#once upload works
+prices = []
+
+for i in range(1,360):
+    galdol = workbook(i,1) #becasue start indexing at zero
+    prices.append(galdol)
+    
+    
+january = 0
+year = 12
+annual = []
+
+for i in range(1,30):        
+    yravg = np.mean(january:year)
+    annual.append(yravg)
+    
+final = np.zeros[30,2]
+final[:,1] == 1987 + arange(30)
+
+final[:,2] == annual
+
+
+#then figure out how to send back to xls
+    
